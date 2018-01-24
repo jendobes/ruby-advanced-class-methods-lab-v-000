c6b5b5bf3dee61675d886fb65a99be7f81324ff9
Jennifer Dobak <jdobak1@gmail.com>
2018-01-24 20:23:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ def self.create_by_name(song_name)
 end
 
 def self.find_by_name(song_name)
-    self.all.detect {|song| song == song_name}
+    self.all.detect {|song| song.name == song_name}
 end
 
 
